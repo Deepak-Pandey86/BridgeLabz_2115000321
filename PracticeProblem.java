aeb14170a41c8b7557c0ebefcb52d8fae20863cc
Deepak-Pandey86 <8630deepakpandey@gmail.com>
2025-01-27 13:43:19 +0530

    19-01-2025

---
+++ PracticeProblem.java
@@ -0,0 +1,16 @@
+class EvenOdd{
+	public static void main(String args[]){
+		for(int i=1;i<=10;i++){
+			if(i%2==0){ 
+			System.out.print(i+" ");
+}
+}
+	        for(int i=1;i<=10;i++)
+{
+	if(i%2!=0){
+        System.out.print(i+" ");
+}
+		}
+	}
+}
+
