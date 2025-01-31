8f58909689000c0c67422dc78f63a6cb68961e46
Deepak-Pandey86 <8630deepakpandey@gmail.com>
2025-01-31 12:13:12 +0530

    31-01-2025

---
+++ Circle.java
@@ -0,0 +1,33 @@
+// Circle class
+public class Circle {
+    private double radius;
+
+    // Constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    // Method to calculate area
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    // Method to calculate circumference
+    public double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    // Method to display details
+    public void displayDetails() {
+        System.out.println("Circle Details:");
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + calculateArea());
+        System.out.println("Circumference: " + calculateCircumference());
+    }
+
+    public static void main(String[] args) {
+        // Creating a Circle object
+        Circle circle = new Circle(5.0);
+        circle.displayDetails();
+    }
+}
